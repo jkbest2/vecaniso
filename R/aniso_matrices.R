006d8ac6f95c8b2a9c7ce75a06fc49dfef5fac32
John Best <jkbest@gmail.com>
2022-10-12 17:01:57 -0700

    Add some general documentation and package deps

--- R/aniso_matrices.R
+++ R/aniso_matrices.R
@@ -3,6 +3,20 @@ det1_gamma <- function(beta) {
   1/2 * (sqrt(beta^2 + 4) - beta)
 }
 
+#' @title Construct anisotropy matrix
+#'
+#' Constructs an anisotropy matrix \code{H} as a mixture of an isotropic effect
+#' and an anisotropic effect. The direction of the anisotropic component is
+#' determined by \code{theta}, and the its intensity by \code{beta}. The
+#' isotropic mixture component is determined by \code{gamma}. Because the
+#' marginal variance of the random field will vary with the determinant of
+#' \code{H}, it is also possible to calculate \code{gamma} so that the
+#' determinant of \code{H} is always one.
+#'
+#' @param theta Primary direction of anisotropy (radians)
+#' @param beta Anisotropic mixture component or intensity
+#' @param gamma Isotropic mixture component; optional
+#' @param det1 Use value of `gamma` so that H has determinant 1?
 #' @export
 aniso_h <- function(theta, beta, gamma = NULL, det1 = TRUE) {
   if (is.null(gamma) && det1) {
