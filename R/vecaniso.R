421077aff1190f514a7350ec89d06911de76d25c
John Best <jkbest@gmail.com>
2022-12-21 09:43:50 -0800

    Add package docs and load vecaniso lib

---
+++ R/vecaniso.R
@@ -0,0 +1,11 @@
+#' Vector field based spatially varying anisotropy
+#'
+#' A package for fitting spatiotemporal abundance models on spatial domains
+#' where anisotropy direction and intensity varies throughout. Anisotropy
+#' direction and relative intensity are derived from existing raster or vector
+#' fields.
+#'
+#' @docType package
+#' @name vecaniso
+#' @useDynLib vecaniso
+NULL
